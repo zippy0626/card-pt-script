22fac28174258c71acd0c4c511cf17b95bbbaa90
zippy0626 <zippy0626@gmail.com>
2025-07-11 11:32:55 -0400

    refactored while loop logic

--- PTCards.py
+++ PTCards.py
@@ -8,24 +8,23 @@ def getRandomCards(deck):
   else:
     return random.randrange(1, len(deck))
 
-def checkUserOption(option):
+def checkUserOption(option, deck):
   # lambda is an anon function waiting to be executed
   options_map = {
-    "r": getRandomCards,
+    "r": getRandomCards(deck),
     "q": lambda: sys.exit()
   }
   if (option in options_map):
-    if (option == "r"): return options_map[option](deck)
+    if (option == "r"): return options_map[option]
     else: return options_map[option]()
   else:
     return int(option)
 
-
-## initial run
 deck = Deck()
 
-print("\nCards PT\n\nHearts = Squats\nSpades = Push Ups\nClubs = Russian Twists\nDiamonds = Burpies\n")
-print('''Ace = 14 reps
+print("\nCards PT\n\nHearts = Squats\nSpades = Push Ups\nClubs = Russian Twists\nDiamonds = Burpees")
+print('''
+Ace = 14 reps
 King = 13 reps
 Queen = 12 reps
 Jack = 11 reps
@@ -33,40 +32,35 @@ Jack = 11 reps
 eg. Jack of Diamonds = 11 burpees    
 ''')
 
-option = input("Enter number of cards to draw, 'r' for random, 'q' to quit: ")
-print()
-print()
-
-number = checkUserOption(option)
-for i in range(number):
-  card = deck.draw()
-  [amount, exercise] = card.getExercise()
-  print(f"{card}: {amount} {exercise}, {amount * 3} sec plank")
-
-deck.printCardsRemaining()
-##
-
+cardsToDraw = None
+usrOption = None
+timesDrew = 0
 
 ## loop
 while True:
   if (len(deck) == 0):
     print("\nYou finished Card PT!\n")
     sys.exit()
-
-  option = input("Continue? Enter number of cards to draw, 'r' for random, 'q' to quit: ")
-  print()
-  print()
-
+  
+  if (timesDrew >= 1):
+    usrOption = input("Continue? Enter number of cards to draw, 'r' for random, 'q' to quit: ")
+    print("\n")
+  else:
+    usrOption = input("Enter number of cards to draw, 'r' for random, 'q' to quit: ")
+    print("\n")
+  
   try:
-    number = checkUserOption(option)
-    if number < 1 or number > len(deck):
+    cardsToDraw = checkUserOption(usrOption, deck)
+    if (cardsToDraw < 1 or cardsToDraw > len(deck)):
       print(f" Invalid Input. Please enter a number between 1 and {len(deck)}.\n")
       continue
   except ValueError:
     print(" Invalid input. Please enter a number, 'r' for random, 'q' to quit.\n")
     continue
-
-  for i in range(number):
+  
+  timesDrew += 1
+  
+  for i in range(cardsToDraw):
     card = deck.draw()
     [amount, exercise] = card.getExercise()
     print(f"{card}: {amount} {exercise}, {amount * 3} sec plank")
